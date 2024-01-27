0f71fed3a48e52174b405fbea79d0fb64efcd485
ishanz0 <ishankataria0422@gmail.com>
2024-01-27 12:37:25 +0530

    TMDB fetch API, built main container

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO, USER_ICON } from "../utils/constants";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -44,7 +45,7 @@ const Header = () => {
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
         alt="logo"
       />
       {user && (
@@ -52,7 +53,7 @@ const Header = () => {
           <img
             className="w-10 h-10 mr-3"
             alt="usericon"
-            src="https://occ-0-4875-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+            src={USER_ICON}
           />
           <button onClick={handleSignOut} className="font-bold text-white ">
             (Sign Out)
