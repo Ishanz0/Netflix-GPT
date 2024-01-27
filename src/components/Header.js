03d7a5f8a906fd8a30bc2b720595a477b1966aa6
ishanz0 <ishankataria0422@gmail.com>
2024-01-27 23:05:29 +0530

    Multi language support in gpt search page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO, USER_ICON } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES, USER_ICON } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {})
@@ -41,20 +44,38 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleGptSearch = () => {
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-44"
-        src={LOGO}
-        alt="logo"
-      />
+      <img className="w-44" src={LOGO} alt="logo" />
       {user && (
-        <div className="flex p-4">
-          <img
-            className="w-10 h-10 mr-3"
-            alt="usericon"
-            src={USER_ICON}
-          />
+        <div className="flex p-2">
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearch}
+          >
+            {showGptSearch ? "Home Page" : "GPT Search"}
+          </button>
+          <img className="w-10 h-10 mr-3 mt-2" alt="usericon" src={USER_ICON} />
           <button onClick={handleSignOut} className="font-bold text-white ">
             (Sign Out)
           </button>
--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,7 @@ import {
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { BG_URL } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -80,11 +81,7 @@ const Login = () => {
     <div>
       <Header />
       <div className="absolute">
-        <img
-          className=""
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-          alt="bgImage"
-        />
+        <img className="" src={BG_URL} alt="bgImage" />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
